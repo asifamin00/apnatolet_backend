8854f2eef8db391eeafcd30f38f26dc38147d226
asif amin <asifamin00@gmail.com>
2024-03-11 16:46:09 +0530

    #

---
+++ models/userSchema.js
@@ -0,0 +1,40 @@
+const mongoose=require('mongoose')
+const UserSchema=({
+    userFname:{
+        type:String,
+        required:true
+    },
+    userLname:{
+        type:String,
+        required:true
+    },
+    password:{
+        type:String,
+        required:true
+    },
+    email:{
+        type:String,
+        required:true
+    },
+    Phone:{
+        type:Number,
+        required:true
+    },
+    Roll:{
+        type:Number,
+        required:true
+    },
+    status:{
+        type:String,
+        required:true
+    },
+    createdBy:{
+        type:String,
+        required:true
+    },
+
+
+     
+},{timestamps:true})
+
+//Roll.admin=2017,Roll.editer=9012,Roll.field=7210,Roll.agent=2346,Roll.woner=1298,Roll.user=1714,

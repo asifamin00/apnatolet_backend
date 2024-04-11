5b8dee32fce3121d795eea91593a4f4934bddf8a
asif amin <asifamin00@gmail.com>
2024-04-11 22:34:28 +0530

    #

---
+++ public/js/modal_apna.js
@@ -0,0 +1,65 @@
+
+// const exampleModal = document.getElementById('exampleModal')
+// if (exampleModal) {
+//   exampleModal.addEventListener('show.bs.modal', event => {
+//     // Button that triggered the modal
+//     const button = event.relatedTarget
+//     // Extract info from data-bs-* attributes
+//     const recipient = button.getAttribute('data-bs-whatever')
+//     // If necessary, you could initiate an Ajax request here
+//     // and then do the updating in a callback.
+
+//     // Update the modal's content.
+//     const modalTitle = exampleModal.querySelector('.modal-title')
+//     const modalBodyInput = exampleModal.querySelector('.modal-body input')
+
+//     modalTitle.textContent = `New message to ${recipient}`
+//     modalBodyInput.value = recipient
+//   })
+// }
+// const exampleModal2 = document.getElementById('exampleModal2')
+// if (exampleModal2) {
+//   exampleModal2.addEventListener('show.bs.modal', event => {
+//     // Button that triggered the modal
+//     const button = event.relatedTarget
+//     // Extract info from data-bs-* attributes
+//     const recipient = button.getAttribute('data-bs-whatever')
+//     // If necessary, you could initiate an Ajax request here
+//     // and then do the updating in a callback.
+
+//     // Update the modal's content.
+//     const modalTitle = exampleModal2.querySelector('.modal-title')
+//     const modalBodyInput = exampleModal2.querySelector('.modal-body input')
+
+//     modalTitle.textContent = `New message to ${recipient}`
+//     modalBodyInput.value = recipient
+//   })
+// }
+document.getElementById("myButton").addEventListener("click", function() {
+  alert("Button clicked!");
+
+  const form = document.querySelector("#userinfo");
+
+async function sendData() {
+  // Associate the FormData object with the form element
+  const formData = new FormData(form);
+
+  try {
+    const response = await fetch("http://localhost:5000/post", {
+      method: "POST",
+      // Set the FormData instance as the request body
+      body: formData,
+    });
+    console.log(await response.json());
+  } catch (e) {
+    console.error(e);
+  }
+}
+
+// Take over form submission
+form.addEventListener("submit", (event) => {
+  event.preventDefault();
+  sendData();
+});
+
+})
